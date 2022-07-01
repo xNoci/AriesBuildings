660363f562d0ca6999cd6cdb508326b11079a33f
Noci <nocigit@gmail.com>
2022-07-01 11:37:16 +0200

    Added getBookText() method

--- src/main/java/net/tttproject/ariesbuildings/blockhistory/BlockHistoryEntry.java
+++ src/main/java/net/tttproject/ariesbuildings/blockhistory/BlockHistoryEntry.java
@@ -1,13 +1,20 @@
 package net.tttproject.ariesbuildings.blockhistory;
 
 import lombok.Getter;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.tttprojekt.system.ProjectSystem;
-import net.tttprojekt.system.utils.ProjectFuture;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
+import xyz.upperlevel.spigot.book.BookUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class BlockHistoryEntry {
+
+    private static final SimpleDateFormat COMPACT_FORMAT = new SimpleDateFormat("dd.MM HH:mm:ss");
+    private static final SimpleDateFormat DETAILED_FORMAT = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
 
     @Getter private final UUID uuid;
     @Getter private final BlockHistoryAction action;
@@ -21,9 +28,16 @@ public class BlockHistoryEntry {
         this.timestamp = timestamp;
     }
 
-    public ProjectFuture<String> getPlayerName() {
-        return ProjectSystem.getUserAPI().getName(this.uuid);
-    }
+    protected BaseComponent getBookText() {
+        String name = ProjectSystem.getUserAPI().getName(uuid).getUninterruptibly();
+        if (StringUtils.isBlank(name)) {
+            name = "Unknown";
+        }
 
+        return BookUtil.TextBuilder
+                .of(String.format("§7[§c%s§7] §0%s by %s", COMPACT_FORMAT.format(new Date(timestamp)), action.getInfo(), name))
+                .onHover(BookUtil.HoverAction.showText(String.format("§bUUID§7: §b%s\n§bType§7: §b%s\n\n§bTimestamp§7:\n  §b%s\n  %s ms", uuid, material.name(), DETAILED_FORMAT.format(new Date(timestamp)), timestamp)))
+                .build();
+    }
 
 }
