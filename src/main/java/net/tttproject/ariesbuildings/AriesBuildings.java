5c9a78cd94b28ca78aa3bcc0f3e420ca5607e64a
Noci <nocigit@gmail.com>
2022-07-01 11:29:44 +0200

    Added some useful methods (getInstance() and kickPlayer onDisable)

--- src/main/java/net/tttproject/ariesbuildings/AriesBuildings.java
+++ src/main/java/net/tttproject/ariesbuildings/AriesBuildings.java
@@ -1,20 +1,26 @@
 package net.tttproject.ariesbuildings;
 
+import lombok.Getter;
 import net.luckperms.api.event.node.NodeMutateEvent;
 import net.tttproject.ariesbuildings.commands.BlockHistoryCommand;
 import net.tttproject.ariesbuildings.hooks.LuckPermsHook;
 import net.tttproject.ariesbuildings.listeners.*;
 import net.tttproject.ariesbuildings.sql.BlockHistorySQL;
 import net.tttproject.ariesbuildings.utils.AFKHandler;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AriesBuildings extends JavaPlugin {
 
+    @Getter private static AriesBuildings instance;
+
     public static final String PREFIX = "§9AriesBuildings §8» §7";
+    public static final String NO_PERMISSION = "§cI'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.";
 
     @Override
     public void onEnable() {
+        instance = this;
         BlockHistorySQL.create();
 
         AFKHandler.instance().register(this);
@@ -26,6 +32,8 @@ public class AriesBuildings extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        Bukkit.getOnlinePlayers().forEach(player -> player.kickPlayer(PREFIX + "§cThe server will be restarted.\nIt should be available again shortly."));
+
         AFKHandler.instance().stop();
     }
 
