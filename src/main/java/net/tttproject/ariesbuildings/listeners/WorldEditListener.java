84716eed68fe0c3c8c2979a5caf5fb8da69c3ef1
Noci <nocigit@gmail.com>
2022-07-02 19:29:46 +0200

    Added BlockHistory logging for worldedit editsession

--- src/main/java/net/tttproject/ariesbuildings/listeners/WorldEditListener.java
+++ src/main/java/net/tttproject/ariesbuildings/listeners/WorldEditListener.java
@@ -1,10 +1,29 @@
 package net.tttproject.ariesbuildings.listeners;
 
+import com.fastasyncworldedit.core.configuration.Settings;
 import com.fastasyncworldedit.core.event.extent.PasteEvent;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.event.extent.EditSessionEvent;
+import com.sk89q.worldedit.extension.platform.Actor;
+import com.sk89q.worldedit.extent.AbstractDelegateExtent;
+import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.util.eventbus.Subscribe;
+import com.sk89q.worldedit.world.block.BlockStateHolder;
+import net.tttproject.ariesbuildings.blockhistory.BlockHistoryAction;
+import net.tttproject.ariesbuildings.blockhistory.BlockHistoryManager;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+import java.util.UUID;
 
 public class WorldEditListener {
+
+    public WorldEditListener() {
+        Settings.settings().EXTENT.ALLOWED_PLUGINS.add(WorldEditLogExtent.class.getName());
+    }
 
     @Subscribe
     public void handlePaste(PasteEvent event) {
@@ -13,7 +32,38 @@ public class WorldEditListener {
 
     @Subscribe
     public void handlePaste(EditSessionEvent event) {
-        //TODO BlockHistory
+        if (event.getActor() == null) return;
+
+        event.setExtent(new WorldEditLogExtent(event.getExtent(), BukkitAdapter.adapt(event.getWorld()), event.getActor()));
+    }
+
+    private static class WorldEditLogExtent extends AbstractDelegateExtent {
+
+        private final World world;
+        private final Actor actor;
+
+
+        public WorldEditLogExtent(Extent extent, World world, Actor actor) {
+            super(extent);
+
+            this.world = world;
+            this.actor = actor;
+        }
+
+        @Override
+        public <T extends BlockStateHolder<T>> boolean setBlock(BlockVector3 position, T block) throws WorldEditException {
+            onBlockChange(position, block);
+            return super.setBlock(position, block);
+        }
+
+        protected <B extends BlockStateHolder<B>> void onBlockChange(BlockVector3 blockVector, B block) {
+            Location location = BukkitAdapter.adapt(world, blockVector);
+            Material newMaterial = BukkitAdapter.adapt(block.getBlockType().getItemType());
+            UUID uuid = actor.getUniqueId();
+
+            BlockHistoryManager.instance().updateHistory(uuid, BlockHistoryAction.WORLD_EDIT_SET, newMaterial, location);
+        }
+
     }
 
 }
