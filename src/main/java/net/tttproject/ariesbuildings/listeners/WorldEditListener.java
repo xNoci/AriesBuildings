3b634c23afac7cada8688fe12214b904b30d80f3
Noci <nocigit@gmail.com>
2022-07-03 10:16:30 +0200

    WorldEdit //place and //place will now be logged

--- src/main/java/net/tttproject/ariesbuildings/listeners/WorldEditListener.java
+++ src/main/java/net/tttproject/ariesbuildings/listeners/WorldEditListener.java
@@ -2,14 +2,18 @@ package net.tttproject.ariesbuildings.listeners;
 
 import com.fastasyncworldedit.core.configuration.Settings;
 import com.fastasyncworldedit.core.event.extent.PasteEvent;
+import com.fastasyncworldedit.core.math.MutableBlockVector3;
+import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.event.extent.EditSessionEvent;
 import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.extent.AbstractDelegateExtent;
 import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.util.eventbus.Subscribe;
+import com.sk89q.worldedit.world.block.BaseBlock;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
 import net.tttproject.ariesbuildings.blockhistory.BlockHistoryAction;
 import net.tttproject.ariesbuildings.blockhistory.BlockHistoryManager;
@@ -27,7 +31,31 @@ public class WorldEditListener {
 
     @Subscribe
     public void handlePaste(PasteEvent event) {
-        //TODO BlockHistory
+        if (event.getActor() == null || !event.getActor().isPlayer()) return;
+        Clipboard clipboard = event.getClipboard();
+
+        World world = BukkitAdapter.adapt(((EditSession) event.getExtent()).getWorld());
+        UUID uuid = event.getActor().getUniqueId();
+
+        BlockVector3 to = event.getPosition();
+        BlockVector3 origin = clipboard.getOrigin();
+        // To must be relative to the clipboard origin ( player location - clipboard origin ) (as the locations supplied are relative to the world origin)
+        final int relx = to.getBlockX() - origin.getBlockX();
+        final int rely = to.getBlockY() - origin.getBlockY();
+        final int relz = to.getBlockZ() - origin.getBlockZ();
+
+        for (BlockVector3 pos : clipboard) {
+            BaseBlock block = pos.getFullBlock(clipboard);
+            int xx = pos.getX() + relx;
+            int yy = pos.getY() + rely;
+            int zz = pos.getZ() + relz;
+
+            BlockVector3 toLocation = MutableBlockVector3.get(xx, yy, zz);
+            Location location = BukkitAdapter.adapt(world, toLocation);
+            Material material = BukkitAdapter.adapt(block.getBlockType().getItemType());
+
+            BlockHistoryManager.instance().updateHistory(uuid, BlockHistoryAction.WORLD_EDIT_PASTE, material, location);
+        }
     }
 
     @Subscribe
@@ -41,7 +69,6 @@ public class WorldEditListener {
 
         private final World world;
         private final Actor actor;
-
 
         public WorldEditLogExtent(Extent extent, World world, Actor actor) {
             super(extent);
